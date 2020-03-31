c0a1298f1dac1bf3d8a48606585bb3170228e671
Laurens Buijs <laurensb96@hotmail.com>
2020-03-31 20:55:40 +0200

    Add files via upload

--- MBUS.py
+++ MBUS.py
@@ -1,15 +1,16 @@
 class MBUS:
     tcp = b''
     cmd = 0
+    hash = b'\x00\x00'
     dlc = 0
-    arb = b'\x00\x00\x00'
-    can = b''
+    data = b''
 
     def decodeTCP(self):
-        self.arb = self.tcp[0:4]  # arbitration field = 0x00 + CMD + hash code
         self.cmd = self.tcp[1]
+        self.hash = self.tcp[2:4]
         self.dlc = self.tcp[4]
-        self.can = self.tcp[5:5+self.dlc]
+        self.data = self.tcp[5:5+self.dlc]
 
+    # encode TCP stream with received CAN data, extending each data frame with zero bytes to length 8
     def encodeTCP(self):
-        self.tcp = self.arb + self.dlc.to_bytes(1, 'big') + self.can + b'\x00' * (8-self.dlc)
+        self.tcp = self.cmd.to_bytes(2, 'big') + self.hash + self.dlc.to_bytes(1, 'big') + self.data + b'\x00' * (8-self.dlc)
--- MBUStcp.py
+++ MBUStcp.py
@@ -15,34 +15,66 @@ print('Connection address:', address)
 
 bus = can.interface.Bus(bustype='kvaser', channel='0', bitrate=500000)
 
-mbus_rx = MBUS.MBUS()
-mbus_tx = MBUS.MBUS()
+# create MBUS receive and transmit object instances
+# receive side processes data incoming from TCP to CAN bus
+# transmit side processes data incoming from CAN bus to TCP
+mbus_dn = MBUS.MBUS()
+mbus_up = MBUS.MBUS()
+
+def canTX(cmd,hash,data):
+    canMsgTx = can.Message(
+        arbitration_id=int.from_bytes(cmd.to_bytes(2, 'big') + hash, byteorder='big'),
+        data=data,
+        is_extended_id=True)
+    bus.send(canMsgTx)
+
+def programMFX():
+    print('Program MFX loks')
+    # first do lok discovery
+    canMsgTx = can.Message(arbitration_id=int.from_bytes(0x02.to_bytes(2, 'big') + mbus_dn.hash, byteorder='big'),
+                         data=[0x20],
+                         is_extended_id=True)
+    bus.send(canMsgTx)
+
+    canMsgRx = bus.recv()
+    arb = canMsgRx.arbitration_id.to_bytes(4, 'big')
+    cmd = arb[1]
+    dlc = canMsgRx.dlc
+    data = canMsgRx.data
+    if cmd == 0x03 and dlc == 5:
+        print('Lok found with MFX-UID:', data[0:4], 'range =', data[4])
+    # now bind an SID to the MFX-UID
+    canMsgTx = can.Message(arbitration_id=int.from_bytes(0x04.to_bytes(2, 'big') + mbus_dn.hash, byteorder='big'),
+                           data=data[0:4] + 0x02.to_bytes(2, 'big'),
+                           is_extended_id=True)
+    bus.send(canMsgTx)
+
 
 while 1:
     # incoming TCP stream routine
-    mbus_rx.tcp = conn.recv(BUFFER_SIZE)
-    mbus_rx.decodeTCP()  # CAN bus data is extracted from the TCP stream
+    mbus_dn.tcp = conn.recv(BUFFER_SIZE)
+    mbus_dn.decodeTCP()  # CAN bus data is extracted from the TCP stream
 
     # extract the relevant messages for direct processing
-    if mbus_rx.cmd == 0x16:
-        print('Switch accessory #:', mbus_rx.tcp[8], 'to state:', mbus_rx.tcp[9])
-    if mbus_rx.cmd == 0x00 and mbus_rx.dlc == 8:
-        print('Time =', mbus_rx.tcp[10], ':', mbus_rx.tcp[11], '(hr:min)')
+    if mbus_dn.cmd == 0x16:
+        print('Switch accessory #:', mbus_dn.data[0:4], 'to position:', mbus_dn.data[4], 'to state:', mbus_dn.data[5])
+    if mbus_dn.cmd == 0x16 and mbus_dn.data[3] == 0x33:
+        programMFX()
+    if mbus_dn.cmd == 0x00 and mbus_dn.dlc == 8:
+        print('Time =', mbus_dn.data[5], ':', mbus_dn.data[6], '(hr:min)')
 
     # assemble CAN message and transmit
-    canMsgTx = can.Message(arbitration_id=int.from_bytes(mbus_rx.arb, byteorder='big'),
-                         data=mbus_rx.can,
-                         is_extended_id=True)
-    bus.send(canMsgTx)
+    canTX(mbus_dn.cmd, mbus_dn.hash, mbus_dn.data)
 
     # incoming CAN stream routine
     canMsgRx = bus.recv(0)
     if canMsgRx is not None:
         print(canMsgRx)
-        mbus_tx.arb = canMsgRx.arbitration_id.to_bytes(4, 'big')
-        mbus_tx.cmd = mbus_tx.arb[1]
-        mbus_tx.dlc = canMsgRx.dlc
-        mbus_tx.can = canMsgRx.data
-        mbus_tx.encodeTCP()
-        conn.send(mbus_tx.tcp)
+        arb = canMsgRx.arbitration_id.to_bytes(4, 'big')
+        mbus_up.cmd = arb[1]
+        mbus_up.hash = arb[2:4]
+        mbus_up.dlc = canMsgRx.dlc
+        mbus_up.data = canMsgRx.data
+        mbus_up.encodeTCP()
+        conn.send(mbus_up.tcp)
 conn.close()
